e9c83ff7d3e24c306203bff7ce4ddba69b8ae56f
sb1994 <sean94@gmail.com>
2023-01-08 23:23:05 +0000

    Started Python Advanced modules

---
+++ basic/regex_example.py
@@ -0,0 +1,11 @@
+# Regular expressions
+
+import re
+
+
+match = re.search('patter', 'Searching pattern in text ')
+
+print(match.re.pattern)
+print(match.string)
+# gives the index of the pattern
+print(match.start())
