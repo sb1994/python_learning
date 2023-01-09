d8a8f2f55ed2f6855dd3829d3ece909cfe7012ed
unknown <sean94@gmail.com>
2023-01-09 00:26:44 +0000

    Create basic script that imports a package

--- intermidate/hello_world2.py
+++ intermidate/hello_world2.py
@@ -1,3 +1,3 @@
 import sys
 
-print(sys)
+print(sys.path)
--- venv/Scripts/activate.bat
+++ venv/Scripts/activate.bat
@@ -8,7 +8,7 @@ if defined _OLD_CODEPAGE (
     "%SystemRoot%\System32\chcp.com" 65001 > nul
 )
 
-set VIRTUAL_ENV=C:\Users\seanb\Desktop\python_learning\venv
+set VIRTUAL_ENV=C:\Users\Sean\Desktop\python_learning\venv
 
 if not defined PROMPT set PROMPT=$P$G
 
