0d4c1d976b8fd12f1fa995b272480cbb42f5d403
unknown <sean94@gmail.com>
2023-01-21 23:20:44 +0000

    Completed Sequencing Protocol Testing in implementing Collections

--- intermidate/collections/test_sorted_set.py
+++ intermidate/collections/test_sorted_set.py
@@ -41,7 +41,73 @@ class TestContainerProtocol(unittest.TestCase):
         self.assertTrue(5 not in self.s)
 
     def test_negative_not_contained(self):
-        self.assertFalse(5 not in self.s)
+        self.assertFalse(9 not in self.s)
+
+
+class TestSizedProtocol(unittest.TestCase):
+    def test_empty(self):
+        s = SortedSet()
+        self.assertEqual(len(s), 0)
+
+    def test_one(self):
+        s = SortedSet([42])
+        self.assertEqual(len(s), 1)
+
+    def test_ten(self):
+        s = SortedSet(range(10))
+        self.assertEqual(len(s), 10)
+
+    def test_with_duplicates(self):
+        s = SortedSet([5, 5, 5])
+        self.assertEqual(len(s), 1)
+
+# iteratable protocol
+
+
+class TestIterable(unittest.TestCase):
+    def setUp(self):
+        self.s = SortedSet([7, 2, 1, 1, 9])
+
+    def test_iter(self):
+        i = iter(self.s)
+        self.assertEqual(next(i), 1)
+        self.assertEqual(next(i), 2)
+        self.assertEqual(next(i), 7)
+        self.assertEqual(next(i), 9)
+        self.assertRaises(StopIteration, lambda: next(i))
+
+    def test_for_loop(self):
+        index = 0
+        expected = [1, 2, 7, 9]
+
+        for item in self.s:
+            self.assertEqual(item, expected[index])
+            index += 1
+
+
+class TestSequenceProtocol(unittest.TestCase):
+    def setUp(self):
+        self.s = SortedSet([1, 4, 9, 13, 15])
+
+    def test_index_zero(self):
+        self.assertEqual(self.s[0], 1)
+
+    def test_index_four(self):
+        self.assertEqual(self.s[4], 15)
+
+    def test_index_one_beyond_the_end(self):
+        with self.assertRaises(IndexError):
+            self.s[5]
+
+    def test_index_minus_one(self):
+        self.assertEqual(self.s[-1], 15)
+
+    def test_index_minus_five(self):
+        self.assertEqual(self.s[-5], 1)
+
+    def test_index_one_before_the_beginning(self):
+        with self.assertRaises(IndexError):
+            self.s[-6]
 
 
 if __name__ == '__main__':
